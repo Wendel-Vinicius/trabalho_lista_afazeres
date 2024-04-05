d9c9326e9d10c717c98bcf7ccb566be00dec5292
Wendel-Vinicius <wendel-vinicius@live.comm>
2024-04-05 19:15:31 -0300

    atualização trabalho

--- lib/main.dart
+++ lib/main.dart
@@ -4,104 +4,113 @@ void main() {
   runApp(const MeuApp());
 }
 
-class MeuApp extends StatefulWidget {
+class MeuApp extends StatelessWidget {
   const MeuApp({Key? key}) : super(key: key);
 
   @override
-  State<MeuApp> createState() => _MeuAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListaDeTarefas(),
+    );
+  }
 }
 
-class _MeuAppState extends State<MeuApp> {
+class ListaDeTarefas extends StatefulWidget {
+  const ListaDeTarefas({Key? key}) : super(key: key);
+
+  @override
+  _ListaDeTarefasState createState() => _ListaDeTarefasState();
+}
+
+class _ListaDeTarefasState extends State<ListaDeTarefas> {
   final List<Tarefa> _tarefas = [];
   final TextEditingController _controlador = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lista de Afazeres'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _tarefas.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_tarefas[index].descricao),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.edit),
-                          onPressed: () {
-                            _editarTarefa(index); // Chama a função para editar a tarefa
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            _excluirTarefa(index);
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: _controlador,
-                      decoration: const InputDecoration(
-                        hintText: 'Descrição',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.resolveWith<Size>(
-                        (Set<MaterialState> states) {
-                          return const Size(200, 60);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Afazeres'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_tarefas[index].descricao),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          _editarTarefa(context, index);
                         },
                       ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _excluirTarefa(index);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _controlador,
+                    decoration: const InputDecoration(
+                      hintText: 'Descrição',
+                      border: OutlineInputBorder(),
                     ),
-                    child: const Text('Adicionar Tarefa'),
-                    onPressed: () {
-                      if (_controlador.text.isEmpty) {
-                        return;
-                      }
-                      setState(() {
-                        _tarefas.add(
-                          Tarefa(
-                            descricao: _controlador.text,
-                            status: false,
-                          ),
-                        );
-                        _controlador.clear();
-                      });
-                    },
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.resolveWith<Size>(
+                      (Set<MaterialState> states) {
+                        return const Size(200, 60);
+                      },
+                    ),
+                  ),
+                  child: const Text('Adicionar Tarefa'),
+                  onPressed: () {
+                    if (_controlador.text.isEmpty) {
+                      return;
+                    }
+                    setState(() {
+                      _tarefas.add(
+                        Tarefa(
+                          descricao: _controlador.text,
+                          status: false,
+                        ),
+                      );
+                      _controlador.clear();
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
-  void _editarTarefa(int index) {
+  void _editarTarefa(BuildContext context, int index) {
     TextEditingController _controladorEdicao =
         TextEditingController(text: _tarefas[index].descricao);
 
